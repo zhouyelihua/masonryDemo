d336d6426c968efe69d083f0d87c3866b426fd6e
赤鱬 <chiru.yyp@cainiao.com>
2016-02-29 19:14:53 +0800

    masonryDemo

--- demo5/ViewController.h
+++ demo5/ViewController.h
@@ -1,14 +1,15 @@
 //
 //  ViewController.h
-//  demo5
+//  demo2
 //
-//  Created by yeah on 2/29/16.
+//  Created by yeah on 2/26/16.
 //  Copyright © 2016 none. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+- (void) btn1;
 
 
 @end
--- demo5/ViewController.m
+++ demo5/ViewController.m
@@ -1,21 +1,37 @@
 //
 //  ViewController.m
-//  demo5
+//  demo2
 //
-//  Created by yeah on 2/29/16.
+//  Created by yeah on 2/26/16.
 //  Copyright © 2016 none. All rights reserved.
 //
 
 #import "ViewController.h"
-
+#import "STSchoolTaskMessage.h"
+#import "Masonry/Masonry.h"
 @interface ViewController ()
-
+@property (nonatomic,strong)IBOutlet UIButton *bbt;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+  
+    STSchoolTaskMessage* sv=[[STSchoolTaskMessage alloc]init];
+   
+    self.view.backgroundColor=[UIColor clearColor];
+    sv.frame=self.view.bounds;
+    
+        [self.view addSubview:sv];
+    [sv mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.center.equalTo(self.view);
+        make.size.mas_equalTo(CGSizeMake(300, 300));
+        
+    }];
+
+ 
+ 
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -23,5 +39,9 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+- (void) btn1
+{
+    NSLog(@"1112323323");
+    return ;
+}
 @end
