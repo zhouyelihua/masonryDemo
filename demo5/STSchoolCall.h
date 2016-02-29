8818a7cab90b87dad4d95b085345676380fb60de
赤鱬 <chiru.yyp@cainiao.com>
2016-02-29 20:16:57 +0800

    Masonry demo

---
+++ demo5/STSchoolCall.h
@@ -0,0 +1,15 @@
+//
+//  STSchoolCall.h
+//  demo5
+//
+//  Created by yeah on 2/29/16.
+//  Copyright © 2016 none. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STSchoolCall : UIView
+@property(nonatomic,strong) UIButton*call;
+-(void ) setup;
+-(void)btn:(id)x;
+@end
