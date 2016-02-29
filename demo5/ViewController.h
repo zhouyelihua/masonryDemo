c80d98226abfa466180b0ba814a9a25cdfbd28d9
赤鱬 <chiru.yyp@cainiao.com>
2016-02-29 14:03:47 +0800

    Initial Commit

---
+++ demo5/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo5
+//
+//  Created by yeah on 2/29/16.
+//  Copyright © 2016 none. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ demo5/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  demo5
+//
+//  Created by yeah on 2/29/16.
+//  Copyright © 2016 none. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
